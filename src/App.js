956628ef458efd968e12b79c25a10c032870f915
Djulooo <jules.bellegante@my-digital-school.org>
2024-01-13 12:22:12 +0100

    Début du projet + bannière

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
 import './App.css';
 
-function App() {
+const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <div className="banner-container">
+        </div>
       </header>
+
+      <div className="titre">
+        </div>
+
     </div>
   );
-}
+};
 
-export default App;
+export default HomePage;
